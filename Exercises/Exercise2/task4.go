5ca94445b6d6be88d1f40de57e6ebd6b413559dc
KMikalsen <KMikalsen@users.noreply.github.com>
2015-01-22 10:54:40 +0100

    Concurrency works, probably a bad solution?

--- Exercises/Exercise2/task4.go
+++ Exercises/Exercise2/task4.go
@@ -1,28 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "runtime"
+	"fmt"
+	"runtime"
+	"sync"
 )
 
+var mutex = &sync.Mutex{}
 var i int = 0
 
-func countUp() {
-    for k := 0; k <= 1000000; k++ {
-        i += 1
-    }
+
+
+
+func countUp(done chan bool) {
+	for k := 0; k <= 1000000; k++ {
+	    mutex.Lock()
+		i += 1
+		mutex.Unlock()
+	}
+	done<- true
 }
 
-func countDown() {
-    for k := 0; k <= 1000000; k++ {
+func countDown(done chan bool) {
+	for k := 0; k <= 1000001; k++ {
+	    mutex.Lock()
         i -= 1
-    }
+        mutex.Unlock()
+	}
+	done<- true
 }
 
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())
-
-    go countUp()
-    go countDown()
-    fmt.Println(i)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	done1 := make(chan bool, 1)
+	done2 := make(chan bool, 1)
+	
+	go countUp(done1)
+	go countDown(done2)
+	<-done1
+	<-done2
+	fmt.Println(i)
 }
